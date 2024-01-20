4b14677397280ba91b3faccec45990173f9b8eff
student <student@astra>
2024-01-20 12:55:51 +0700

    aaa

--- main2.py
+++ main2.py
@@ -1,25 +1,27 @@
-class Book:
-    def __init__(self, author, numbwepages, yearofrelese):
-        self.__author = author
-        self.__numbwepages = numbwepages
-        self.__yearofrelese = yearofrelese
+class Book: #создаем класс
+    def __init__(self, author, numbwepages, yearofrelese): #создаем конструктор класса с свойствами (author, numbwepages, yearofrelese)
+        self.__author = author #приватный
+        self.__numbwepages = numbwepages #приватный
+        self.__yearofrelese = yearofrelese #приватный
 
-    def get_author(self):
+    def get_author(self): #метод предоставляет доступ к значениям свойств объекта класса Book
         return self.__author
 
-    def set_author(self, author):
+    def set_author(self, author): #метод предоставляет доступ к значениям свойств объекта класса Book
         self.__author = author
 
-    def get_numbwepages(self):
+    def get_numbwepages(self): #метод предоставляет доступ к значениям свойств объекта класса Book
         return self.__numbwepages
 
-    def set_numbwepages(self, numbwepages):
+    def set_numbwepages(self, numbwepages): #метод предоставляет доступ к значениям свойств объекта класса Book
         self.__numbwepages = numbwepages
 
-    def get_yearofrelese(self):
+    def get_yearofrelese(self): #метод предоставляет доступ к значениям свойств объекта класса Book
         return self.__yearofrelese
 
-    def set_yearofrelese(self, yearofrelese):
+    def set_yearofrelese(self, yearofrelese): #метод предоставляет доступ к значениям свойств объекта класса Book
         self.__yearofrelese = yearofrelese
 
-
+book1 = Book ('Евгений Замятин', '288', '1924')
+print(f'Книга1: автор - {book1.get_author()}, количество страниц - {book1.get_numbwepages()}, год выпуска - {book1.get_yearofrelese()}')
+#вывод всех значений
--- main3.py
+++ main3.py
@@ -1,34 +1,35 @@
-class Student:
-    def __init__(self, name, age, group, liter):
-        self.__name = name
-        self.__age = age
-        self.__group = group
-        self.__liter = liter
+class Book:
+    def __init__(self, author: str, numbwepages: int, yearofrelese: int = None):
+        self._author = None
+        self._numbwepages = None
+        self._apartment = None
+        self.author = author
+        self.numbwepages = numbwepages
+        self.yearofrelese = yearofrelese
 
-    def get_name(self):
-        return self.__name
 
-    def set_name(self,name):
-        self.__name = name
+    def author(self):
+        return self._author
 
-    def get_age(self):
-        return self.__age
 
-    def set_age(self,age):
-        self.__age = age
+    def author(self, value):
+        if value.startswith('И. О. Фамилия'):
+            self._author = 'И. О. Фамилия'
+        else:
+            raise ValueError('Неверно введены инициалы')
 
-    def get_group(self):
-        return self.__group
 
-    def set_group(self,group):
-        self.__group = group
+    def numbwepages(self):
+        return self._numbwepages
 
-    def get_liter(self):
-        return self.__liter
 
-    def set_liter(self,liter):
-        self.__liter = liter
-        return sum(liter) / len(liter)
+    def numbwepages(self, value):
+        if self._author == 'И. О. Фамилия' and (value < 10 or value > 50):
+            raise ValueError('Инициалы автора должены быть от 10 до 50 символов')
+        elif self._numbwepages == 'Количество страниц' and (value < 1 or value > 100):
+            raise ValueError('Номер дома на улице должен быть от 5 до 2000')
+        elif self._yearofrelese == 'Год выпуска' and (value < 1500 or value > 2024):
+            raise ValueError('Год выпуска должен быть от 1500 до 2024')
+        else:
+            self._numbwepages = value
 
-stud = Student("Владлен", "18", "АВД(23)-Б", "3,4,5,3,5,4,5,3,5,4,5,3,4,5")
-print(Student.__liter)
